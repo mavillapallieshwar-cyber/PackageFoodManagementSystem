8ce4e8efb6f2a4119a9d0f3b414c3eab4d35e2ed
YayatiNehe <yayatigorakshanath.nehe@cognizant.com>
2025-12-26 18:00:45 +0530

    added customer UI

--- PackageFoodManagementSystem.Application/Controllers/PaymentController.cs
+++ PackageFoodManagementSystem.Application/Controllers/PaymentController.cs
@@ -1,6 +1,43 @@
-﻿namespace PackageFoodManagementSystem.Application.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PackagedFoodFrontend.Controllers // Updated namespace to match Solution Explorer
+
 {
-    public class PaymentController
+
+    public class PaymentController : Controller
+
     {
+
+        [HttpGet]
+
+        public IActionResult Checkout()
+
+        {
+
+            return View(); // Refers to Views/Payment/Checkout.cshtml
+
+        }
+
+        [HttpPost]
+
+        public IActionResult Confirm(string method)
+
+        {
+
+            TempData["PaymentMethod"] = method;
+
+            return RedirectToAction("Success");
+
+        }
+
+        public IActionResult Success()
+
+        {
+
+            return View(); // Refers to Views/Payment/Success.cshtml
+
+        }
+
     }
+
 }
--- PackageFoodManagementSystem.Application/Controllers/UserController.cs
+++ PackageFoodManagementSystem.Application/Controllers/UserController.cs
@@ -1,6 +1,116 @@
-﻿namespace PackageFoodManagementSystem.Application.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using PackagedFoodFrontend.Models;
+
+namespace PackagedFoodFrontend.Controllers
 {
-    public class UserController
+    public class UserController : Controller
     {
+        private static Wallet _wallet = new Wallet { Balance = 0 };
+
+        public IActionResult MyWallet()
+
+        {
+
+            return View(_wallet);
+
+        }
+
+        [HttpPost]
+
+        public IActionResult AddMoney(decimal amount)
+
+        {
+
+            _wallet.Balance += amount;
+
+            return RedirectToAction("MyWallet");
+
+        }
+
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult MyBasket()
+        {
+            return View();
+        }
+        public IActionResult MyOrders()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult Logout() => RedirectToAction("Index", "Home");
+
+        public IActionResult EditProfile()
+        {
+            return View();
+        }
+        public IActionResult DeliveryAddress()
+        {
+            return View();
+        }
+        public IActionResult EmailAddress()
+        {
+            return View();
+        }
+        public IActionResult SmartBasket()
+        {
+            return View();
+        }
+        public IActionResult PastOrders()
+        {
+            return View();
+        }
+        public IActionResult GiftCards()
+        {
+            return View();
+        }
+
+        public IActionResult AddAddress()
+
+        {
+
+            return View();
+
+        }
+        public IActionResult Payment()
+        {
+            return View();
+        }
+
+
+        // Handle Add Address form submission (POST)
+
+        [HttpPost]
+
+        public IActionResult AddAddress(string street, string city, string pin)
+
+        {
+
+            // For now, just simulate saving the address
+
+            TempData["Message"] = $"New address added: {street}, {city}, {pin}";
+
+            return RedirectToAction("DeliveryAddress");
+
+        }
+        public IActionResult AddGiftCard()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddGiftCard(string cardNumber, decimal amount, string expiry)
+        {
+            // For now, just simulate saving the card
+            TempData["Message"] = $"Gift card added: {cardNumber} ₹{amount} – Expires: {expiry}";
+            return RedirectToAction("GiftCards");
+        }
+
+
     }
 }
